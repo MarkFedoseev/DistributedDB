32f8b4bc6268cae7025f3ee57b9f27064d106335
MarkFedoseev <undermousem@gmail.com>
2023-06-15 20:56:29 +0300

    deleteDup_and_clientModification

--- src/client.py
+++ src/client.py
@@ -2,43 +2,55 @@ import requests
 import json
 import argparse
 
-# Create the parser
-parser = argparse.ArgumentParser(description='Make a CRUD operation on the server.')
+class Client:
+    def __init__(self, serverAddress):
+        self.server = serverAddress
+    def create(self, key, value):
+        return requests.post(self.server, json={"key": key, "value": value})
+    def read(self, key):
+        return requests.get(self.server + "?key=" + key)
+    def update(self, key, value):
+        return requests.put(self.server, json={"key": key, "value": value})
+    def delete(self, key):
+        return requests.delete(self.server + "?key=" + key)
 
-# Add the arguments
-parser.add_argument('serverAddress', type=str, help='Address of the server.')
-parser.add_argument('operation', type=str, help='The operation to perform: create, read, update, delete.')
-parser.add_argument('key', type=str, help='The key for the operation.')
-parser.add_argument('--value', type=str, help='The value for create or update operation.')
+if __name__ == '__main__':
 
-# Parse the arguments
-args = parser.parse_args()
+    parser = argparse.ArgumentParser(description='Make a CRUD operation on the server.')
 
-# Server URL
-#url = "http://localhost:8080"
-if args.serverAddress:
-    url = args.serverAddress.lower()
+    parser.add_argument('serverAddress', type=str, help='Address of the server.')
+    parser.add_argument('operation', type=str, help='The operation to perform: create, read, update, delete.')
+    parser.add_argument('key', type=str, help='The key for the operation.')
+    parser.add_argument('--value', type=str, help='The value for create or update operation.')
 
-#class Client:
-#    def __init__():
-# Perform the specified operation
-if args.operation.lower() == 'create':
-    if args.value is None:
-        print("Error: You must specify a value for create operation.")
-    else:
-        response = requests.post(url, json={"key": args.key, "value": args.value})
+    args = parser.parse_args()
+
+    if args.serverAddress:
+        url = args.serverAddress.lower()
+
+    client = Client(url)
+
+    if args.operation.lower() == 'create':
+        if args.value is None:
+            print("Error: You must specify a value for create operation.")
+        else:
+            response = client.create(args.key, args.value)
+            print(response.json())
+
+    elif args.operation.lower() == 'read':
+        response = client.read(args.key)
         print(response.json())
-elif args.operation.lower() == 'read':
-    response = requests.get(url + "?key=" + args.key)
-    print(response.json())
-elif args.operation.lower() == 'update':
-    if args.value is None:
-        print("Error: You must specify a value for update operation.")
-    else:
-        response = requests.put(url, json={"key": args.key, "value": args.value})
+
+    elif args.operation.lower() == 'update':
+        if args.value is None:
+            print("Error: You must specify a value for update operation.")
+        else:
+            response = client.update(args.key, args.value)
+            print(response.json())
+
+    elif args.operation.lower() == 'delete':
+        response = client.delete(args.key)
         print(response.json())
-elif args.operation.lower() == 'delete':
-    response = requests.delete(url + "?key=" + args.key)
-    print(response.json())
-else:
-    print("Error: Invalid operation. Valid operations are create, read, update, delete.")
+        
+    else:
+        print("Error: Invalid operation. Valid operations are create, read, update, delete.")
